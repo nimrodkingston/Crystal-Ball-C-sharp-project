a7472a6d8e411bf076ea943c3d06df1471595f09
nimrodkingston <55467605+nimrodkingston@users.noreply.github.com>
2023-02-17 20:12:23 +0000

    Initial development of the backend code
    This is an initial build of the backend code which can communicate via api calls in order to use chatGPT3 capabilities. This code can allow for the behaviour of responses to change based on user choice with parameters such as length of response, how the response should be changed if certain words are used and general themes.

---
+++ API-Call.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+async Task<String> GetResponse(String prompt ,HttpClient client)
+{
+    using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.openai.com/v1/completions"))
+    {
+        request.Headers.TryAddWithoutValidation("Authorization", "Bearer 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");
+        request.Content = new StringContent("{\"model\": \"text-davinci-003\",\"prompt\":" + prompt + ",\"temperature\":0,\"max_tokens\":1000}");
+        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+        using (var response = await client.SendAsync(request))
+        {
+            using (HttpContent content = response.Content)
+            {
+                dynamic data = JObject.Parse(content.ReadAsStringAsync().GetAwaiter().GetResult());
+                return(data.choices[0].text);
+            }
+        }
+    }
+}
+
+HttpClient client = new HttpClient();
+String responseBehaviour = "Respond to the following prompt dramatically as if you are a mysterious fortune teller infront of a crystal ball.If the word you is used the prompt is referring to the fortune teller.If the prompt seems trivial or makes fun of you act as if your time is being wasted. Keep the response under 100 words.";
+
+while (true) {
+    Console.WriteLine("Ask the crystal ball whatever the heart desires and it will show you what you are looking for\n");
+
+    String input = "\"" + responseBehaviour + Console.ReadLine() + "\"";
+    String response = await GetResponse(input,client);
+    Console.WriteLine(response);
+}
+
