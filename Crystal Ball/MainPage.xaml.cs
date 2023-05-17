732e53d749992e1b3c297233abcb75184723f177
nimrodkingston <nimrodkingston@yahoo.co.uk>
2023-05-17 16:28:11 +0100

    Remote client-side/server-side additions
    I have moved the API calls from the client side application to the java server which handles converting JSON and returning string outputs

--- Crystal Ball/MainPage.xaml.cs
+++ Crystal Ball/MainPage.xaml.cs
@@ -1,48 +1,40 @@
 ﻿namespace Crystal_Ball;
+using System.Text;
 using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Sockets;
 
 public partial class MainPage : ContentPage
 {
     //FEATURE NOTE (1): I should add a system to tell if the user asks a question or asks for an image and I can use an analysis of the inputted text via API calls to do this, meaning that the output type can be entirely worked out within the get response method
     //FEATURE NOTE(2): I can also make it so that the image is savable to the device if the user chooses. To do this I will have to look into memory streams and change the API response to b64, meaning a few other things will have to change too 
     //FEATURE(2) ADDITIONAL: This change can potentially make displaying images faster as they do not have to be downloaded to be displayed by the image object. Will have to compare URL download speed vs b64 decryption speed
-    HttpClient client = new HttpClient();
+    IPEndPoint destination = new(IPAddress.Parse("ENTER SERVER IP HERE"), 70);
     Boolean outputType = true;
-    String responseBehaviour = "Respond to the following prompt dramatically as if you are a mysterious fortune teller infront of a crystal ball.If the word you is used the prompt is referring to the fortune teller.If the prompt asks you for a joke act as if your time is being wasted and be snarky. Keep the response under 100 words.";
     public MainPage()
     {
         InitializeComponent();
         Layout.Children.Remove(OutputImage);
     }
-    async Task<String> GetResponse(String prompt, HttpClient client, Boolean outputType)
+    async Task<String> GetResponse(String prompt, Boolean outputType)
     {
-
-        using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.openai.com/v1/" + (outputType ? "completions" : "images/generations")))
+        using (Socket clientSocket = new(destination.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
         {
-
-            request.Headers.TryAddWithoutValidation("Authorization", "Bearer ENTER OPENAI API KEY HERE");
-            request.Content = new StringContent(outputType ? "{\"model\": \"text-davinci-003\",\"prompt\":" + prompt + ",\"temperature\":0,\"max_tokens\":1000}" : "{\"prompt\":" + prompt + ",\"size\":\"512x512\"}");
-            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
-
-            using (var response = await client.SendAsync(request))
-            {
-                using (HttpContent content = response.Content)
-                {
-                    dynamic output = JObject.Parse(content.ReadAsStringAsync().GetAwaiter().GetResult()); //This part will probably have to change as the JSON output is different between the two different cURL requests
-                    String answer = outputType ? output.choices[0].text : output.data[0].url; // I can put this straight into the return later but I am leaving it like this for breakpoint reasons
-                    OutputLabel.Text = answer;
-                    return (answer);
-                }
-            }
+            await clientSocket.ConnectAsync(destination);
+            byte[] data = Encoding.ASCII.GetBytes(prompt);
+            clientSocket.Send(data);
+            byte[] buffer = new byte[1024];
+            clientSocket.Receive(buffer);
+            return Encoding.ASCII.GetString(buffer);
         }
     }
     private async void onTextCompleted(object sender, EventArgs e)
     {
         clearScreen();
         ProgressLabel.Text = "The haze within the crystal ball swirls...";
-        String input = outputType ? "\"" + responseBehaviour + TextInput.Text + "\"" : "\"" + TextInput.Text + "\"";
-        String response = await GetResponse(input, client, outputType);
+        String input = TextInput.Text;
+        String response = await GetResponse(input, outputType);
 
         if (outputType)
         {
---
+++ Server/src/Server.java
@@ -0,0 +1,58 @@
+import java.net.*;
+import java.io.*;
+import java.lang.ProcessBuilder;
+import org.json.JSONObject;
+
+public class Server {
+    //The accept method for the server socket will only accept one connection at a time so I should look into using threads to manage multiple connections
+    // The api call now works but escaping escape characters is really messy looking- maybe look into using okttp now that maven is working
+    // Now that I have moved the api calls to the serverside it would be nice to have AI generated images outputted too
+    public static void main(String[] args){
+        try{
+            String recievedMessage = "";
+            String responseBehaviour = "Respond to the following prompt dramatically as if you are a mysterious fortune teller infront of a crystal ball.If the word you is used the prompt is referring to the fortune teller.If the prompt asks you for a joke act as if your time is being wasted and be snarky. Keep the response under 20 words:";
+            ServerSocket serverConnection = new ServerSocket(70);
+                System.out.println("Server socket established sucessfully");
+
+            while (recievedMessage != "exit"){
+                
+                Socket clientConnection = serverConnection.accept();
+                System.out.println("Client side connection succesful");
+
+                InputStream inputStream = clientConnection.getInputStream();
+                byte[] buffer = new byte[1024];
+                int length = inputStream.read(buffer);
+                recievedMessage = responseBehaviour + new String(buffer,0,length);
+                String command = "curl https://api.openai.com/v1/completions -H \"Content-Type: application/json\" -H \"Authorization: Bearer ENTER API KEY HERE\" -d \"{\\\"model\\\": \\\"text-davinci-003\\\", \\\"prompt\\\": \\\"" + recievedMessage + "\\\", \\\"max_tokens\\\": 30, \\\"temperature\\\": 0}";
+                ProcessBuilder commandBuilder = new ProcessBuilder(command.split(" "));
+                
+                try{
+                    Process commandProcess = commandBuilder.start();
+                    InputStream apiResponse = commandProcess.getInputStream();
+                    BufferedReader inputReader = new BufferedReader(new InputStreamReader(apiResponse));
+                    JSONObject json = new JSONObject(inputReader.readLine());
+                    String response = new JSONObject(json.getJSONArray("choices").get(0).toString()).getString("text").trim();
+                    byte[] data = response.getBytes();
+                    OutputStream socketOutput = clientConnection.getOutputStream();
+                    socketOutput.write(data);
+                    System.out.println(response);
+                }
+
+                catch(Exception e){
+                    System.out.println("There was a problem with recieving the JSON from the openai api");
+                    System.out.println(e);
+                }
+
+                clientConnection.close();
+                System.out.println("Client connection closed");
+            }
+
+            System.out.println("Server connection has been closed");
+            
+        }
+        catch (Exception e){
+            System.out.println("There was an error establishing an active socket");
+            System.out.println(e);
+        }
+    }
+}
