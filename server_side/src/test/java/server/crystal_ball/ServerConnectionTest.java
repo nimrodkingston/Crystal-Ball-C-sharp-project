4b10c194f7f578f479aa25195ccfdf3aad4bf394
nimrodkingston <nimrodkingston@yahoo.co.uk>
2023-08-24 20:51:09 +0100

    Changed file name for clarity

--- server_side/src/test/java/server/crystal_ball/ServerConnectionTest.java
+++ server_side/src/test/java/server/crystal_ball/ServerConnectionTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class UnitTests {
+public class ServerConnectionTest {
     
     @Test
     void testServerConnection(){
